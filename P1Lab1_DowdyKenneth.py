63c8b48292c03d8abf3a5b70ea31309c5a43155c
Kenneth Dowdy <dowdyk9566@student.faytechcc.edu>
2026-09-22 22:00:37 -0400

    Add files via upload

---
+++ P1Lab1_DowdyKenneth.py
@@ -0,0 +1,8 @@
+#Kenneth Dowdy
+# September 22, 2026
+# P1Lab1
+# Displays a greeting message to the user.
+
+first_name = input("First name: ")
+last_name = input("Last name: ")
+print(f"Hello, {first_name} {last_name} Welcome to CTI-110!")
